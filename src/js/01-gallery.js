2b149883c7282ae3a7d526f2a275d86c87dc4dc4
Kryvko97 <108485653+Kryvko97@users.noreply.github.com>
2023-07-20 20:50:46 +0300

    1

---
+++ src/js/01-gallery.js
@@ -0,0 +1,35 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+const gallery = document.querySelector('.gallery')
+const itemsMarkup = createGalleryImagesMarkup (galleryItems)
+
+// Створюємо галерею картинок з масиву обєктів
+function createGalleryImagesMarkup (galleryItems) {
+return galleryItems.map(({ preview, original, description }) => {
+    return `
+    <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>
+`;
+})
+.join('');
+}
+
+gallery.insertAdjacentHTML('beforeend', itemsMarkup)
+
+// Підключаємо бібліотеку Lightbox
+// Ставимо підписи на картинки з затримкою показу
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+})
